83fc5180adfd7af24338b52ed4387f8055467864
Kai Lee <Kai Lee>
2016-03-01 09:17:57 +0800

    Initial commit

---
+++ src/chapter14/FilledList.java
@@ -0,0 +1,34 @@
+package chapter14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 泛化的Class引用
+ * Created by Kai on 2016/2/29.
+ */
+class CountedInteger {
+    private static long counter;
+    private final long id = counter++;
+    public String toString() {
+        return Long.toString(id);
+    }
+}
+public class FilledList<T> {
+    private Class<T> type;
+    public FilledList(Class<T> type) { this.type = type; }
+    public List<T> create(int nElements) {
+        List<T> result = new ArrayList<T>();
+        try {
+            for(int i = 0; i < nElements; i++)
+                result.add(type.newInstance()); //Class.newInstance 返回类的确切类型
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        FilledList<CountedInteger> fl = new FilledList<CountedInteger>(CountedInteger.class);
+        System.out.println(fl.create(15));
+    }
+}
