83ff75b1dbd369e2cbe1423d5bc89cc29b50f22a
KaiLee <likainwpu@163.com>
2016-03-23 23:20:07 +0800

    leetcode

---
+++ src/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Created by KaiLee on 2016/3/23.
+ */
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] colCheck = new int[9][9];
+        int[][] rowCheck = new int[9][9];
+        int[] matrixCheck = new int[9];
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                for(int k = 0; k < 9; k++) {
+
+                }
+            }
+        }
+    }
+    public static void main(String... args) {
+
+    }
+}
