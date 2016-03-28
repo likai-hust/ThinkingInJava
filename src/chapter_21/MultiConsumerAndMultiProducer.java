46d2416b82137bea3a494eef1c063fc9cf6248c1
KaiLee <likainwpu@163.com>
2016-03-28 23:01:29 +0800

    unfinished

---
+++ src/chapter_21/MultiConsumerAndMultiProducer.java
@@ -0,0 +1,11 @@
+package chapter_21;
+
+/**
+ * 多个生产者和消费者仿真模型
+ * Created by KaiLee on 2016/3/28.
+ */
+public class MultiConsumerAndMultiProducer {
+    public static void main(String... args) {
+
+    }
+}
